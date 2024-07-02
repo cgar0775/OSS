5cbaf694264489c79a36c4733c3a517c6977e738
cgar0775 <cgar0775@yahoo.com>
2024-07-02 13:29:01 -0400

    Login page setup with additions to run.py code base

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/login', methods = ['GET','POST'])
+
+def login():
+    if request.method == 'POST':
+
+        #handling login logic
+        username = request.form['username']
+        password = request.form['password']
+        #not sure what this exactly does coppied from chatgpt
+        return redirect(url_for('index'))
+    
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

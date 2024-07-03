68d319a9c70a4d91913e237c24f2ab881403fe5e
cgar0775 <cgar0775@yahoo.com>
2024-07-02 22:19:29 -0400

    Business and Customer signup pages created

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ def hello_world():
     return render_template('index.html')
 
 @app.route('/login', methods = ['GET','POST'])
-
 def login():
     if request.method == 'POST':
 
@@ -18,6 +17,31 @@ def login():
         return redirect(url_for('index'))
     
     return render_template('login.html')
+
+@app.route('/Bsignup', methods = ['GET','POST'])
+def Bsignup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        address = request.form['address']
+        return redirect(url_for('index'))
+    
+    return render_template('Bsignup.html')
+
+
+@app.route('/Csignup', methods = ['GET','POST'])
+def Csignup():
+    if request.method == 'POST':
+         username = request.form['username']
+         password = request.form['password']
+         businessname = request.form['business name']
+         address = request.form['address']
+         services = request.form['service offerings']
+         return redirect(url_for('index'))
+    
+    return render_template('Csignup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
